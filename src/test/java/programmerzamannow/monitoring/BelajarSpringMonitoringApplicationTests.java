d191b630c72fb0f5d85de38f4afa8e1285ba90b4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-08 14:22:13 +0700

    belajar spring monitoring

---
+++ src/test/java/programmerzamannow/monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
